05f4bfa60127a299197e800fadb9e6709c6f4d2b
21anubhav <156325504+21anubhav@users.noreply.github.com>
2025-04-11 08:44:10 +0530

    Add files via upload

---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=1;i<6;i++){
+        for (int j=2;j<=i+1;j++)
+        cout<<i;
+        {
+        cout<<endl;
+        }
+        
+    }
+}
