acea837ce57367631a66772302d67e761e86f668
21anubhav <156325504+21anubhav@users.noreply.github.com>
2025-06-13 11:16:24 +0530

    Add files via upload

---
+++ strings advance/iterating.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str = "anubhavpathania";
+    for(int i=0; i<str.length();i++){
+        cout<<str[i];
+        cout<<endl;
+    }
+}
